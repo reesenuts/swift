11fd0fad216605485407deff1222d5a9501eca18
0.8lili <130875766+reesenuts@users.noreply.github.com>
2025-05-04 10:04:33 +0800

    sidebar (frontend + routes fixed)

---
+++ src/routes/admin/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Sidebar from "$lib/admin/sidebar.svelte";
+</script>
+
+<div class="flex flex-row h-screen w-full">
+    <Sidebar />
+    <slot />
+</div>
---
+++ src/routes/admin/user-management/+page.svelte
@@ -0,0 +1,7 @@
+<div class="flex flex-row w-full">
+    <div class="w-full h-screen flex flex-col items-center justify-center">
+        <div class="flex flex-col items-center justify-center h-screen">
+            <h1 class="text-4xl font-bold text-[#1e1e1e]">Welcome to the User Management</h1>
+        </div>
+    </div>
+</div>
