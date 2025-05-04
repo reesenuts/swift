511db4078a602d17d81c70f59c783ee15ac9cfd5
0.8lili <130875766+reesenuts@users.noreply.github.com>
2025-05-04 14:02:17 +0800

    user-management (frontend)

---
+++ src/lib/admin/users-item.svelte
@@ -0,0 +1,80 @@
+<!-- table head -->
+<div class="w-full flex justify-between items-center p-4 rounded-lg gap-4 border-1 border-[#EBEBE8] mb-2">
+    <p class="text-sm text-[#818181] font-medium pl-2 w-[5%]">001</p>
+    <!-- username -->
+    <div  class="text-sm text-[#818181] font-medium pl-2 w-[25%] flex items-center gap-4">
+        <!-- avatar -->
+        <div class="h-10 w-10 rounded-full text-sm flex items-center justify-center font-semibold text-[#443C68] bg-[#443c6836]">
+            R
+        </div>
+        <!-- username and email -->
+        <div class="text-xs">
+        <p class="text-[#443C68] font-semibold">reesenuts</p>
+        <p class="text-[#A5A4A1]">reesenuts@gmail.com</p>
+        </div>
+    </div>
+    <!-- role -->
+    <div class="text-sm text-[#FFFFFF] w-[10%] p-2 rounded-lg bg-[#443c68d2] text-center">
+        Admin
+    </div>
+    <!-- status -->
+    <div class="text-sm text-[#6C9B7D] font-medium w-[15%] bg-[#DBEDDB] p-2 text-center rounded-lg ml-10">
+        Active
+    </div>
+    <!-- date created -->
+    <p class="text-sm text-[#818181] font-medium w-[15%] pl-10">
+        Apr 15, 2025
+    </p>
+    <!-- actions -->
+    <div class="flex gap-2 w-[20%]">
+        <button class="text-sm text-[#E16F64] p-2 rounded-lg font-medium bg-[#FFE2DD] cursor-pointer hover:bg-[#e16e643f] transition duration-200 ease-in-out w-full">Disable</button>
+        <button class="text-sm text-[#818181] font-medium w-full p-2 rounded-lg flex items-center gap-2 justify-center bg-[#FFE2DD] cursor-pointer hover:bg-[#e16e643f] transition duration-200 ease-in-out">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#E16F64" viewBox="0 0 256 256">
+                <path d="M216,48H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM192,208H64V64H192ZM80,24a8,8,0,0,1,8-8h80a8,8,0,0,1,0,16H88A8,8,0,0,1,80,24Z"></path>
+            </svg>
+            <p class="text-[#E16F64]">Delete</p>
+        </button>
+    </div>
+</div>
+
+<!-- table head -->
+<div class="w-full flex justify-between items-center p-4 rounded-lg gap-4 border-1 border-[#EBEBE8] mb-2">
+    <p class="text-sm text-[#818181] font-medium pl-2 w-[5%]">002</p>
+    <!-- username -->
+    <div  class="text-sm text-[#818181] font-medium pl-2 w-[25%] flex items-center gap-4">
+        <!-- avatar -->
+        <div class="h-10 w-10 rounded-full text-sm flex items-center justify-center font-semibold text-[#443C68] bg-[#443c6836]">
+            A
+        </div>
+        <!-- username and email -->
+        <div class="text-xs">
+        <p class="text-[#443C68] font-semibold">andrei.mayo</p>
+        <p class="text-[#A5A4A1]">andrei.mayo@gmail.com</p>
+        </div>
+    </div>
+    <!-- role -->
+    <div class="text-sm text-[#443C68] w-[10%] font-medium p-2 rounded-lg bg-[#443c681c] text-center">
+        User
+    </div>
+    <!-- status -->
+    <div class="text-sm text-[#91918E] font-medium w-[15%] bg-[#E3E2E0] p-2 text-center rounded-lg ml-10">
+        Inactive
+    </div>
+    <!-- date created -->
+    <p class="text-sm text-[#818181] font-medium w-[15%] pl-10">
+        May 04, 2025
+    </p>
+    <!-- actions -->
+    <div class="flex gap-2 w-[20%]">
+        <button class="text-sm text-[#6C9B7D] p-2 rounded-lg font-medium bg-[#DBEDDB] cursor-pointer hover:bg-[#6c9b7d53] transition duration-200 ease-in-out w-full">
+            Enable
+        </button>
+        <button class="text-sm text-[#818181] font-medium w-full p-2 rounded-lg flex items-center gap-2 justify-center bg-[#FFE2DD] cursor-pointer hover:bg-[#e16e643f] transition duration-200 ease-in-out">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#E16F64" viewBox="0 0 256 256">
+                <path d="M216,48H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM192,208H64V64H192ZM80,24a8,8,0,0,1,8-8h80a8,8,0,0,1,0,16H88A8,8,0,0,1,80,24Z"></path>
+            </svg>
+            <p class="text-[#E16F64]">Delete</p>
+        </button>
+    </div>
+</div>
+  
--- src/routes/admin/user-management/+page.svelte
+++ src/routes/admin/user-management/+page.svelte
@@ -1,7 +1,46 @@
-<div class="flex flex-row w-full">
-    <div class="w-full h-screen flex flex-col items-center justify-center">
-        <div class="flex flex-col items-center justify-center h-screen">
-            <h1 class="text-4xl font-bold text-[#1e1e1e]">Welcome to the User Management</h1>
+<script lang="ts">
+  import UsersItem from "$lib/admin/users-item.svelte";
+</script>
+
+<div class="w-full">
+      <div class="p-6 flex flex-col justify-between gap-6">
+        <!-- path -->
+        <div class="flex items-center gap-2">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#443C68" viewBox="0 0 256 256">
+            <path d="M213.85,125.46l-112,120a8,8,0,0,1-13.69-7l14.66-73.33L45.19,143.49a8,8,0,0,1-3-13l112-120a8,8,0,0,1,13.69,7L153.18,90.9l57.63,21.61a8,8,0,0,1,3,12.95Z"></path>
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#B8BCBC" viewBox="0 0 256 256">
+            <path d="M181.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L164.69,128,90.34,53.66a8,8,0,0,1,11.32-11.32l80,80A8,8,0,0,1,181.66,133.66Z"></path>
+          </svg>
+          <h1 class="text-sm text-[#B8BCBC]">General</h1>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#B8BCBC" viewBox="0 0 256 256">
+            <path d="M181.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L164.69,128,90.34,53.66a8,8,0,0,1,11.32-11.32l80,80A8,8,0,0,1,181.66,133.66Z"></path>
+          </svg>
+          <h1 class="text-sm text-[#443C68] font-medium">User Management</h1>
         </div>
+    
+        <!-- headline -->
+        <div>
+          <h1 class="text-2xl font-bold text-[#443C68]">User Management</h1>
+          <h1 class="text-sm text-[#A5A4A1]">
+            Manage your swift users and their account permissions here.
+          </h1>
+        </div>
+      </div>
+  
+      <!-- notifications -->
+      <div class="p-6 w-full">
+      <!-- table head -->
+      <div class="w-full flex justify-between items-center bg-[#FCFCFA] p-4 rounded-lg gap-4 border-1 border-[#EBEBE8] mb-2">
+        <p class="text-sm text-[#818181] font-medium pl-2 w-[5%]">ID</p>
+        <p class="text-sm text-[#818181] font-medium pl-2 w-[25%]">Username</p>
+        <p class="text-sm text-[#818181] font-medium w-[10%]">Role</p>
+        <p class="text-sm text-[#818181] font-medium w-[15%] ml-10">Status</p>
+        <p class="text-sm text-[#818181] font-medium w-[15%] pl-10">Date Created</p>
+        <p class="text-sm text-[#818181] font-medium w-[20%]">Actions</p>
+      </div>
+      <!-- users items -->
+      <UsersItem />
     </div>
-</div>
+  </div>
+  
