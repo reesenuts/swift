df08d63325a0eb22604bca682d4c3e9990a108c9
0.8lili <130875766+reesenuts@users.noreply.github.com>
2025-05-04 11:35:44 +0800

    dashboard (frontend)

---
+++ src/lib/admin/notification-item.svelte
@@ -0,0 +1,27 @@
+<!-- table header -->
+<div class="w-full flex justify-between items-center p-4 rounded-lg gap-4 border-1 border-[#EBEBE8]">
+  <!-- username -->
+  <div  class="text-sm text-[#818181] font-medium pl-2 w-[25%] flex items-center gap-4">
+    <!-- avatar -->
+    <div class="h-10 w-10 rounded-full text-sm flex items-center justify-center font-semibold text-[#443C68] bg-[#443c6836]">
+      A
+    </div>
+    <!-- username and email -->
+    <div class="text-xs">
+      <p class="text-[#443C68] font-semibold">andrei.mayo</p>
+      <p class="text-[#A5A4A1]">andrei.mayo@gmail.com</p>
+    </div>
+  </div>
+  <!-- message -->
+  <div class="text-sm text-[#443C68] w-[40%] font-medium p-2 rounded-lg bg-[#443c681c] truncate">
+    Reminder. You have a meeting with the client at 3:00 PM. Please be on time.
+  </div>
+  <!-- schedule -->
+  <p class="text-sm text-[#818181] font-medium w-[20%] pl-10">
+    May 04, 2025 11:26 AM
+  </p>
+  <!-- status -->
+  <div class="text-sm text-[#6C9B7D] font-medium w-[15%] bg-[#DBEDDB] p-2 text-center rounded-lg">
+    Sent
+  </div>
+</div>
